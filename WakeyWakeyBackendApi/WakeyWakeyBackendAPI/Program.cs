ca1bda15d1611a2c14b973b92c2ce9c05281e02b
Bryan Zavala <noexprtees@gmail.com>
2026-01-14 19:59:41 -0600

    config: ensure that generated json responses follow the camel case convention
    Swift frontend code will expect the json objects to be in camel case.

--- WakeyWakeyBackendApi/WakeyWakeyBackendAPI/Program.cs
+++ WakeyWakeyBackendApi/WakeyWakeyBackendAPI/Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.DataProtection;
@@ -30,7 +31,15 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
     //.AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
 
-builder.Services.AddControllers();
+// Controllers.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // Explicitly specify that JSON messages follow the camel case convention
+        // as that is what is expected by the Swift frontend.
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
