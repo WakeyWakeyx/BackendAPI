90987e01af2bcaa80b5940983c12a58eaae7b777
Bryan Zavala <noexprtees@gmail.com>
2026-02-08 22:22:47 -0600

    feature: begin work on active alarm database schema

---
+++ WakeyWakeyBackendApi/WakeyWakeyBackendAPI/Models/Alarm.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WakeyWakeyBackendAPI.Models;
+
+/// <summary>
+/// Represents an active alarm as set by a specific user.
+/// </summary>
+public class Alarm
+{
+    /// <summary>
+    /// The unique identifier of this alarm.
+    /// </summary>
+    [Key]
+    public int AlarmId { get; set; }
+    
+    /// <summary>
+    /// the unique identifier of the user who set this alarm.
+    /// </summary>
+    [ForeignKey(nameof(User))]
+    public int UserId { get; set; }
+    
+    /// <summary>
+    /// The time to wake the user at.
+    /// </summary>
+    [Required]
+    public required DateTime WakeTime { get; set; }
+    
+    /// <summary>
+    /// User navigation property.
+    /// </summary>
+    public virtual required User User { get; set; }
+}
