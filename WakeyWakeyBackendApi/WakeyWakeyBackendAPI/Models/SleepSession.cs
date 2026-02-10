7e26c6f5ce8c3cbf09e80742f18b240769273cf2
Bryan Zavala <noexprtees@gmail.com>
2026-02-09 23:03:57 -0600

    docs: write some basic docs for sleep sessions

--- WakeyWakeyBackendApi/WakeyWakeyBackendAPI/Models/SleepSession.cs
+++ WakeyWakeyBackendApi/WakeyWakeyBackendAPI/Models/SleepSession.cs
@@ -1,28 +1,33 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WakeyWakeyBackendAPI.Models
+namespace WakeyWakeyBackendAPI.Models;
+
+/// <summary>Represents a single instance of a user's sleep records.</summary>
+[Table("SleepSessions")]
+public class SleepSession
 {
-    [Table("SleepSessions")]
-    public class SleepSession
-    {
-        [Key]
-        [Required]
-        public required int SessionId { get; set; }
-        
-        [Required]
-        [ForeignKey(nameof(User))]
-        public required int UserId { get; set; }
-        
-        [Required]
-        public required DateTime StartTime { get; set; }
-        
-        [Required]
-        public required DateTime EndTime { get; set; }
-        
-        [Required]
-        public required int SleepScore { get; set; }
-        
-        public virtual required User User { get; set; }
-    }
+    /// <summary>The unique id of this sleep session.</summary>
+    [Key]
+    [Required]
+    public int SessionId { get; set; }
+    
+    /// <summary>The unique id of the user this sleep session applies to.</summary>
+    [Required]
+    [ForeignKey(nameof(User))]
+    public required int UserId { get; set; }
+    
+    /// <summary>The bedtime of this sleep session.</summary>
+    [Required]
+    public required DateTime StartTime { get; set; }
+    
+    /// <summary>The wake time of this sleep session.</summary>
+    [Required]
+    public required DateTime EndTime { get; set; }
+    
+    /// <summary>The sleep score assigned to this sleep session.</summary>
+    [Required]
+    public required int SleepScore { get; set; }
+    
+    public virtual User User { get; set; }
 }
