932475b410d8b54e4e07fd557b0c99c9b56e9e4a
Bryan Zavala <noexprtees@gmail.com>
2026-02-16 11:01:27 -0600

    refactor: rename sleep session time intervals to match created diagrams

---
+++ WakeyWakeyBackendApi/WakeyWakeyBackendAPI/Migrations/20260216165951_RenameSleepSessionTimeIntervals.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WakeyWakeyBackendAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameSleepSessionTimeIntervals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StartTime",
+                table: "SleepSessions",
+                newName: "WakeTime");
+
+            migrationBuilder.RenameColumn(
+                name: "EndTime",
+                table: "SleepSessions",
+                newName: "BedTime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WakeTime",
+                table: "SleepSessions",
+                newName: "StartTime");
+
+            migrationBuilder.RenameColumn(
+                name: "BedTime",
+                table: "SleepSessions",
+                newName: "EndTime");
+        }
+    }
+}
