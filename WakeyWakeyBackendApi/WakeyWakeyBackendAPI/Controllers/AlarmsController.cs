3c9f81ebeac577ecf98011787ff8fad4b81d2f37
Bryan Zavala <noexprtees@gmail.com>
2026-02-16 18:19:43 -0600

    refactor: various tweaks to alarm docs and names

--- WakeyWakeyBackendApi/WakeyWakeyBackendAPI/Controllers/AlarmsController.cs
+++ WakeyWakeyBackendApi/WakeyWakeyBackendAPI/Controllers/AlarmsController.cs
@@ -11,11 +11,11 @@ namespace WakeyWakeyBackendAPI.Controllers;
 /// <summary>Handles management of alarms set by users.</summary>
 [Route("api/[controller]")]
 [ApiController]
-public class AlarmController : ControllerBase
+public class AlarmsController : ControllerBase
 {
     private readonly AppDbContext _context;
     
-    public AlarmController(AppDbContext context)
+    public AlarmsController(AppDbContext context)
     {
         _context = context;
     }
@@ -24,22 +24,22 @@ public class AlarmController : ControllerBase
     /// <param name="alarmId">the numeric id of the alarm.</param>
     /// <returns>The alarm details, if any.</returns>
     [Authorize]
-    [HttpGet("getAlarm")]
-    public async Task<ActionResult<ExistingAlarmDto>> GetAlarm([FromQuery] int alarmId)
+    [HttpGet("{alarmId:int}")]
+    public async Task<ActionResult<ExistingAlarmDto>> GetAlarm([FromRoute] int alarmId)
     {
         var userId = User.GetUserId();
         if (userId == null)
             return Unauthorized();
-        var alarm = await GetAlarmEntity(userId.Value, alarmId);
-        if (alarm == null)
+        var alarmEntity = await GetAlarmEntity(userId.Value, alarmId);
+        if (alarmEntity == null)
             return NotFound();
-        return CreateAlarmDto(alarm);
+        return CreateAlarmDto(alarmEntity);
     }
 
     /// <summary>Returns all alarms currently set by the given user.</summary>
     /// <returns>A list containing all alarms set by `userId`.</returns>
     [Authorize]
-    [HttpGet("getAlarms")]
+    [HttpGet]
     public async Task<ActionResult<List<ExistingAlarmDto>>> GetAlarms()
     {
         var userId = User.GetUserId();
@@ -55,25 +55,27 @@ public class AlarmController : ControllerBase
     /// <summary>Registers a new alarm set by a user.</summary>
     /// <param name="alarmDto">The alarm details provided by user.</param>
     [Authorize]
-    [HttpPost("createAlarm")]
-    public async Task<ActionResult<FreshAlarmDto>> CreateAlarm([FromBody] FreshAlarmDto alarmDto)
+    [HttpPost]
+    public async Task<ActionResult<ExistingAlarmDto>> CreateAlarm([FromBody] FreshAlarmDto alarmDto)
     {
         var userId = User.GetUserId();
         if (userId == null)
             return Unauthorized();
-        _context.Alarms.Add(CreateAlarmEntity(userId.Value, alarmDto));
+        
+        // Create and save the new alarm.
+        var alarmEntity = CreateAlarmEntity(userId.Value, alarmDto);
+        _context.Alarms.Add(alarmEntity);
         await _context.SaveChangesAsync();
-        return alarmDto;
+        return CreateAlarmDto(alarmEntity);
     }
 
     /// <summary>Updates attributes of an existing alarm with those provided in the request body.</summary>
     /// <param name="alarmId">The unique identifier of the alarm.</param>
     /// <param name="alarmDto">The new alarm details to set.</param>
     [Authorize]
-    [HttpPatch("updateAlarm")]
-    public async Task<ActionResult> UpdateAlarm([FromQuery] int alarmId, [FromBody] UpdatedAlarmDto alarmDto)
+    [HttpPatch("{alarmId:int}")]
+    public async Task<ActionResult<ExistingAlarmDto>> UpdateAlarm([FromRoute] int alarmId, [FromBody] UpdatedAlarmDto alarmDto)
     {
-        // TODO: Need to remove these ugly things.
         var userId = User.GetUserId();
         if (userId == null)
             return Unauthorized();
@@ -96,39 +98,47 @@ public class AlarmController : ControllerBase
             alarmEntity.DaysToRepeat = alarmDto.DaysToRepeat.Value;
         _context.Alarms.Update(alarmEntity);
         await _context.SaveChangesAsync();
-        return Ok();
+        return CreateAlarmDto(alarmEntity);
     }
 
     /// <summary>Deletes an alarm previously created by a user.</summary>
     /// <param name="alarmId">The unique id of the alarm</param>
     /// <returns>OK if the alarm was found and successfully deleted.</returns>
     [Authorize]
-    [HttpDelete("deleteAlarm")]
-    public async Task<ActionResult> DeleteAlarm([FromQuery] int alarmId)
+    [HttpDelete("{alarmId:int}")]
+    public async Task<ActionResult> DeleteAlarm([FromRoute] int alarmId)
     {
         var userId = User.GetUserId();
         if (userId == null)
             return Unauthorized();
-        // Try finding the alarm
-        var toDelete = await GetAlarmEntity(userId.Value, alarmId);
-        if (toDelete == null)
+        
+        // Try finding the alarm.
+        var alarmEntity = await GetAlarmEntity(userId.Value, alarmId);
+        if (alarmEntity == null)
             return NotFound();
-        // Then delete it
-        _context.Alarms.Remove(toDelete);
+        
+        // Then delete it.
+        _context.Alarms.Remove(alarmEntity);
         await _context.SaveChangesAsync();
         return Ok();
     }
     
+    /// <summary>Helper method for finding a specific alarm attached to a user.</summary>
+    /// <param name="userId">The id of the user.</param>
+    /// <param name="alarmId">The id of the alarm.</param>
+    /// <returns>The alarm entity, or null if none  could be found.</returns>
     private async Task<Alarm?> GetAlarmEntity(int userId, int alarmId)
     {
         return await _context.Alarms
-            .Where(alarm => alarm.UserId == userId && alarm.Id == alarmId)
-            .FirstOrDefaultAsync();
+            .FirstOrDefaultAsync(alarm => alarm.UserId == userId && alarm.Id == alarmId);
     }
 
+    /// <summary>Helper method for converting an Alarm entity to an ExistingAlarmDto.</summary>
+    /// <param name="alarm">The Alarm entity.</param>
+    /// <returns>An ExistingAlarmDto with the same details as the entity.</returns>
     private static ExistingAlarmDto CreateAlarmDto(Alarm alarm)
     {
-        return new ExistingAlarmDto()
+        return new ExistingAlarmDto
         {
             Id = alarm.Id,
             Name = alarm.Name,
@@ -139,6 +149,10 @@ public class AlarmController : ControllerBase
         };
     }
 
+    /// <summary>Helper method for converting a FreshAlarmDto to an Alarm entity.</summary>
+    /// <param name="userId">The id of the user who created the alarm.</param>
+    /// <param name="alarmDto">The details of the newly-created alarm.</param>
+    /// <returns>An Alarm entity with the same details as those in the FreshAlarmDto.</returns>
     private static Alarm CreateAlarmEntity(int userId, FreshAlarmDto alarmDto)
     {
         return new Alarm
